e9def8daecab017228e114b58eb5340c6afac467
Mohit Agrawal <149875811+Gadgetechno@users.noreply.github.com>
2025-11-15 23:49:28 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,12 +23,12 @@ export default function Home() {
   ]
 
   const quickLinks = [
-    { img: githubLogo, title: 'GitHub', link: 'https://github.com/kunj2803' },
-    { img: linkedinLogo, title: 'LinkedIn', link: 'https://www.linkedin.com/in/kunj-desai-07717b293/' },
-    { img: gmailLogo, title: 'Email', link: 'mailto:kunjd2803@gmail.com' },
-    { img: whatsappLogo, title: 'WhatsApp', link: 'https://wa.me/+918758209508' },
-    { img: instagramLogo, title: 'Instagram', link: 'https://www.instagram.com/kunj_2834/' },
-    { img: facebookLogo, title: 'Facebook', link: 'https://www.facebook.com/kunj.desai.222608' },
+    { img: githubLogo, title: 'GitHub', link: 'https://github.com/Gadgetechno' },
+    { img: linkedinLogo, title: 'LinkedIn', link: 'https://www.linkedin.com/in/mohitagrawalcse/' },
+    { img: gmailLogo, title: 'Email', link: 'mailto:mohitagrawal.alpine10.com' },
+    { img: whatsappLogo, title: 'WhatsApp', link: 'https://wa.me/+916367536765' },
+    { img: instagramLogo, title: 'Instagram', link: 'https://www.instagram.com/mohit_agrawal__25/' },
+    { img: facebookLogo, title: 'Facebook', link: 'https://www.facebook.com/mohit' },
   ]
 
   return (
@@ -85,13 +85,13 @@ export default function Home() {
               transition={{ duration: 4, repeat: Infinity, ease: 'linear' }}
               className="home-name"
             >
-              Kunj Desai
+              Mohit Agrawal
             </motion.span>
           </h1>
 
           {/* Typing Animated Text */}
           <p className="typing-effect">
-            Artificial Intelligence Engineer | Frontend Developer | Tech Explorer
+            Artificial Intelligence Engineer | Software Engineer Aspirant | Tech Explorer
           </p>
 
           {/* Profession Tags */}
